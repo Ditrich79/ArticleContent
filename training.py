108833a4c821b8e8f49255c189fdde407bbf0950
Ditrich79 <adk18@mail.ru>
2025-08-20 16:47:21 +0300

    Fix CI/CD

--- training.py
+++ training.py
@@ -1,17 +1,5 @@
 import pytest
 
 
-# def get_reversed_string(string: str):
-#     return  string[::-1]
-#
-#
-# @pytest.mark.parametrize(
-#     "value, result", [
-#         ('abrakadabra', 'adrakadabra'),
-#         ('sheetyman', ["s", "h", "e", "e", "t", "y", "m", "a", "n"])
-#     ]
-# )
-# def test_get_reversed_string(value, result):
-#     assert get_reversed_string(value) == result
 
 
